cb01dcf5306eb0b1feed4c238cc71321984a7f0c
persona64 <54071237+persona64@users.noreply.github.com>
2019-08-16 14:08:18 +0300

    Add files via upload
    search in lines

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "zero one zero Jack one superUser";
+            var result = data.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(x => x)
+                .Where(x => x.Count() == 1)
+                .Select(x => x.Key);
+            foreach (var item in result) Console.Write(item + " ");
+            Console.ReadLine();
+        }
+    }
+}
