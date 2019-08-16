03ef267cd97a7129b30e745457c524d31042b9bf
persona64 <54071237+persona64@users.noreply.github.com>
2019-08-16 14:10:20 +0300

    Add files via upload
    Нахождение факториала числа

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ciclenum = 10;
+            int x = 5;
+            int fact = 1;
+
+            for (int i = x; i <= ciclenum; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("Факториал от " + x + " до " + ciclenum + " = " + fact);
+            Console.ReadLine();
+        }
+    }
+}
