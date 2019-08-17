9b99a6e883101f253e8bd6c28b7b8a9f5bdf49c2
persona64 <54071237+persona64@users.noreply.github.com>
2019-08-17 18:40:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bracketsCheck
+{
+    class Program
+    {
+        class BracketsChecker
+        {
+
+            private readonly string _opening = "([{"; 
+            private readonly string _closing = ")]}";
+
+            private bool _cantBeBalanced;
+
+            private Stack<int> _opened = new Stack<int>();  
+
+            public bool Balanced => !_cantBeBalanced && !_opened.Any();
+
+            public void Put(char ch)    
+            {
+                if (_cantBeBalanced) return;
+
+                int index = _opening.IndexOf(ch);
+
+                if(index != -1)
+                {
+                    _opened.Push(index);
+                    return;
+                }
+
+                index = _closing.IndexOf(ch);
+
+                if(index != -1)
+                {
+                    if(!_opened.Any() || _opened.Peek() != index)
+                    {
+                        _cantBeBalanced = true;
+                        _opened.Clear();
+                        _opened.TrimExcess();
+                        return;
+                    }
+
+                    _opened.Pop();
+                    return;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var str = "{},(),[]";
+            var checker = new BracketsChecker();
+
+            foreach (var ch in str)
+            {
+                checker.Put(ch);
+            }
+
+            Console.WriteLine(checker.Balanced);
+            Console.ReadLine();
+        }
+    }
+}
